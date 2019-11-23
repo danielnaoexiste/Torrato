2c53c9201e3d351462da7cf0eca4eeb60d08d3b9
danielnaoexiste <danieldenardo1@gmail.com>
2019-11-23 16:07:42 -0300

    Grid Movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends KinematicBody2D
 
 # Grid and Speed vars
 var speed : int = 256
@@ -9,16 +9,40 @@ var last_position : Vector2 = Vector2()
 var target_position : Vector2 = Vector2()
 var movedir : Vector2 = Vector2()
 
+var tween_x : int = 0
+var tween_y : int = 0
+
+var can_move : bool = true;
+
+# Onready vars
+onready var ray : RayCast2D = $RayCast2D
+onready var fall_tween : Tween = $Sprite/FallTween
+onready var fall_timer : Timer = $FallTimer
+
 # Initialize snapped to grid
 func _ready():
 	position = position.snapped(Vector2(tile_size, tile_size))
 	last_position = position
 	target_position = position
-
+	
 # Handles Movement
 func _process(delta):
-	position += speed * movedir * delta
-	
+	if ray.is_colliding():
+		position = last_position
+		target_position = last_position
+		if movedir != Vector2.ZERO:
+			can_move = false
+			
+			fall_tween.interpolate_property($Sprite, "position", 
+			Vector2(0, 0), Vector2(tween_x, tween_y),
+			.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			
+			
+			fall_tween.start()
+			fall_timer.start(.5)
+	elif can_move:
+		position += speed * movedir * delta
+		
 	# Snaps the player if moved more than the intended
 	if position.distance_to(last_position) >= tile_size:
 		position = target_position
@@ -43,3 +67,19 @@ func _get_movedir():
 	# Prevents Diagonals
 	if movedir.x != 0 and movedir.y != 0:
 		movedir = Vector2.ZERO
+	
+	# Move raycast to edge of the player in the movedir
+	if movedir != Vector2.ZERO:
+		ray.cast_to = movedir * tile_size / 2
+		if can_move:
+			tween_x = movedir.x * tile_size
+			tween_y = movedir.y * tile_size
+
+# Returns the tween after fall
+func _on_Timer_timeout():
+	fall_timer.stop()
+	fall_tween.interpolate_property($Sprite, "position", Vector2(tween_x, tween_y), 
+	Vector2(0, 0), .5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	fall_tween.start()
+	yield(fall_tween, "tween_all_completed")
+	can_move = true;
