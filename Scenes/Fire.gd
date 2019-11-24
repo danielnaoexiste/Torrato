d1d7140a23f810c51e50b09e7aeb772890d0d526
danielnaoexiste <danieldenardo1@gmail.com>
2019-11-24 16:52:52 -0300

    fire in level 1/2

--- Scenes/Fire.gd
+++ Scenes/Fire.gd
@@ -1,9 +1,21 @@
 extends Area2D
 
-export(String, FILE, "*.tscn") var next_scene
+onready var timer : Timer = $Timer
+onready var col : CollisionShape2D = $CollisionShape2D
 
+func _ready():
+	timer.start(3)
+	col.disabled = true;
+	visible = not visible
+	
 func _process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			scene_changer.change_scene(next_scene)
+			get_tree().reload_current_scene()
+
+func _on_Timer_timeout():
+	col.disabled = not col.disabled
+	visible = not visible
+	timer.stop()
+	timer.start(3)
